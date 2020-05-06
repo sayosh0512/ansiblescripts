883a10165312df4732ec6f1d8ef932ebcdcffd4d
sayosh0512 <63592135+sayosh0512@users.noreply.github.com>
2020-05-06 12:00:38 +0530

    Update ansiblemain.sh

--- ansiblemain.sh
+++ ansiblemain.sh
@@ -8,10 +8,15 @@ echo "decoded text : ${decotext}" >> /home/"${1}"/log.txt
 
 clonerepo(){
   cd /home/"${1}"/ 
-  wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/moodleinstall.sh
+  wget https://raw.githubusercontent.com/sayosh0512/ansibles-wordpress-script/master/ansibleserversetup.sh
   sudo chown -R "${1}":"${1}" /home/"${1}"/moodleinstall.sh
 }
 clonerepo ${1} >> /home/"${1}"/log.txt
+
+moodlesetup()
+    git clone https://github.com/sayosh0512/moodle_playbook.git
+    ansible-playbook /home/azureadmin/moodle_playbook/playbook.yml -i /etc/ansible/hosts -u azureadmin
+
 
   cat <<EOF > /home/"${1}"/run.sh
   #!/bin/bash
